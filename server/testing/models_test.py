efb8be6babe3fb6f26ec624d8518c206198f8ffd
Oliver cheruiyot <olivercher1000@gmail.com>
2025-01-20 13:30:07 +0300

    updated to handle delete message from database

--- server/testing/models_test.py
+++ server/testing/models_test.py
@@ -1,32 +1,26 @@
 from datetime import datetime
-
 from app import app
 from models import db, Message
 
 class TestMessage:
     '''Message model in models.py'''
 
-    with app.app_context():
-        m = Message.query.filter(
-            Message.body == "Hello 👋"
-            ).filter(Message.username == "Liza")
+    def setup_method(self, method):
+        with app.app_context():
+            db.create_all()
+            message = Message(body="Hello 👋", username="Liza")
+            db.session.add(message)
+            db.session.commit()
 
-        for message in m:
-            db.session.delete(message)
-
-        db.session.commit()
+    def teardown_method(self, method):
+        with app.app_context():
+            db.session.remove()
+            db.drop_all()
 
     def test_has_correct_columns(self):
         '''has columns for message body, username, and creation time.'''
         with app.app_context():
-
-            hello_from_liza = Message(
-                body="Hello 👋",
-                username="Liza")
-            
-            db.session.add(hello_from_liza)
-            db.session.commit()
-
-            assert(hello_from_liza.body == "Hello 👋")
-            assert(hello_from_liza.username == "Liza")
-            assert(type(hello_from_liza.created_at) == datetime)
+            hello_from_liza = Message.query.first()
+            assert hello_from_liza.body == "Hello 👋"
+            assert hello_from_liza.username == "Liza"
+            assert type(hello_from_liza.created_at) == datetime
